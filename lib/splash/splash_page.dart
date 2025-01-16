2613434d5bfdf7c6ba23eb1506bd968891606ddc
amr mustafa <amrmustafa6051@gmail.com>
2025-01-16 13:39:00 +0200

    finish splash page logic - navigate to home page

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:github_review_2/main.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyHomePage(title: "Flutter")),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
